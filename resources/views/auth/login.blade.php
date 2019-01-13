354e0ac8e5e0e3fc4c6657166e3c1cdb8381ec39
Chris Franklin <chris@cwfranklin.com>
2019-01-13 09:50:01 +0000

    Adding username error feedback

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -15,11 +15,17 @@
                             <label for="email" class="col-sm-4 col-form-label text-md-right">{{ __('Username') }} /<br />{{ __('E-Mail Address') }}</label>
 
                             <div class="col-md-6">
-                                <input id="email" type="text" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="text" class="form-control{{ $errors->has('email') || $errors->has('username') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required autofocus>
 
                                 @if ($errors->has('email'))
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+
+                                @if ($errors->has('username'))
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $errors->first('username') }}</strong>
                                     </span>
                                 @endif
                             </div>
