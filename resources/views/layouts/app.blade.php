ad7f6d2362e1c215e7ebe5c2c7a53346a625e6f5
Chris Franklin <chris@cwfranklin.com>
2019-01-13 15:35:31 +0000

    Adding profile and control panel links to menu

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,6 +55,12 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ url('/users/' . Auth::user()->id) }}">My Profile</a>
+
+                                    @if (Auth::user()->role->site_admin)
+                                        <a class="dropdown-item" href="">Control Panel</a>
+                                    @endif
+
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
