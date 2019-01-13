f93e97ab0d5c215d8bbc24a19bb9bed707471191
Chris Franklin <chris@cwfranklin.com>
2019-01-13 14:14:36 +0000

    Updating role field names and adding Role relationship & model

--- database/migrations/2019_01_11_000000_create_roles_table.php
+++ database/migrations/2019_01_11_000000_create_roles_table.php
@@ -16,26 +16,26 @@ class CreateRolesTable extends Migration
         Schema::create('roles', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name');
-            $table->boolean('has_site_admin')->default(0);
-            $table->boolean('has_gallery_admin')->default(0);
+            $table->boolean('site_admin')->default(0);
+            $table->boolean('gallery_admin')->default(0);
             $table->timestamps();
         });
 
         DB::table('roles')->insert([
             [
                 'name' => 'Super Admin',
-                'has_site_admin' => true,
-                'has_gallery_admin' => true
+                'site_admin' => true,
+                'gallery_admin' => true
             ],
             [
                 'name' => 'Gallery Admin',
-                'has_site_admin' => false,
-                'has_gallery_admin' => true
+                'site_admin' => false,
+                'gallery_admin' => true
             ],
             [
                 'name' => 'User',
-                'has_site_admin' => false,
-                'has_gallery_admin' => false
+                'site_admin' => false,
+                'gallery_admin' => false
             ]
         ]);
     }
