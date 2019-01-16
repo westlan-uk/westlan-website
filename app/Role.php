af07cb7193fc9822a47d7e3b1dedde9bd45ce31d
Chris Franklin <chris@cwfranklin.com>
2019-01-16 21:37:57 +0000

    Updating includes for JS and CSS compiling. Adding Sweetalert2

--- app/Role.php
+++ app/Role.php
@@ -10,6 +10,6 @@ class Role extends Model
 
     public function users()
     {
-    	return $this->hasMany('App\User');
+        return $this->hasMany('App\User');
     }
 }
--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -19,17 +19,18 @@ try {
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
-
+/*
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+*/
 
 /**
  * Next we will register the CSRF Token as a common header with Axios so that
  * all outgoing HTTP requests automatically have it attached. This is just
  * a simple convenience so we don't have to attach every token manually.
  */
-
+/*
 let token = document.head.querySelector('meta[name="csrf-token"]');
 
 if (token) {
@@ -37,6 +38,7 @@ if (token) {
 } else {
     console.error('CSRF token not found: https://laravel.com/docs/csrf#csrf-x-csrf-token');
 }
+*/
 
 /**
  * Echo exposes an expressive API for subscribing to channels and listening
